8d4889f5875c8aeb4de0b162d0bc93a630fda46b
Silletr <hristicenkoigor764@gmail.com>
2025-11-09 14:33:26 +0200

    [ARCHITECTURE UPDATE] Updated architecture of project for more convenience in development and access to Store.nvim plugin

--- lua/GPTCode.nvim/python/main.py
+++ lua/GPTCode.nvim/python/main.py
@@ -1,8 +1,10 @@
 from g4f.client import Client
 
 client = Client()
+query = input("Enter query to AI: ")
+
 response = client.chat.completions.create(
-    model="gpt-4o", messages=[{"role": "user", "content": "Hi!"}]
+    model="gpt-4o", messages=[{"role": "user", "content": query}]
 )
 
 print(response.choices[0].message.content)
